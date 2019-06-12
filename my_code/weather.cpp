89d2fcd883b2c0372c62b3d9dd388f5698f76f43
Da Yeon <dayeonshin0315@gmail.com>
2019-06-12 12:36:05 -0400

    weather

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -4,10 +4,77 @@
 
 using namespace std;
 
-ostream& operator<<(std::ostream& os, const GPS& gps) {
-	os << gps.latitude << " " << gps.longitude << endl;
-	return os;
+const double F_TO_C = 5 / 9;
+const double C_TO_F = 9 / 5;
+
+Image::Image(int w, int h, std::string flnm)
+    : width(w), height(h)
+{
+    filename = flnm;
+    image_buf = new char[image_sz()];
 }
+
+// copy constructor:
+Image::Image(const Image& img2) {
+	height = img2.height;
+	width = img2.width;
+	filename = img2.filename;
+	image_buf = new char[image_sz()];
+ 	for (int i = 0; i < image_sz(); i++) {
+ 		image_buf[i] = img2.image_buf[i];
+		}
+ }
+
+Image::~Image() {
+	if (image_buf != nullptr)
+		delete image_buf;
+}
+
+ Image& Image::operator=(const Image& img2) {
+ 	if (&img2 != this) {
+ 		if (image_buf != nullptr)
+ 			delete image_buf;
+ 		height = img2.height;
+ 		width = img2.width;
+ 		filename = img2.filename;
+ 		image_buf = new char[image_sz()];
+ 		for (int i = 0; i < image_sz(); i++) {
+ 			image_buf[i] = img2.image_buf[i];
+ 		}
+ 	}
+ 	return *this;
+ }
+
+int Image::image_sz() {
+    return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+}
+
+
+    /*
+     * Setting `display() = 0` here makes this an abstract
+     * class that can't be implemented.
+     * */
+string Image::display(std::string s) {
+    return "Displaying image " + s;
+}
+
+
+
+Date::Date(int d, int m, int y) {
+    day = d;
+    month = m;
+    year = y;
+}
+
+
+double WReading::get_tempF() {
+    return (temperature * C_TO_F) + 32;
+}
+
 
 /*
  * A constructor for weather class.
@@ -16,41 +83,9 @@ Weather::Weather(std::string nm, GPS loc) :
     station_nm(nm), my_loc(loc) {
 }
 
-Date::Date(int d, int m, int y) :
-    day(d), month(m), year(y) {
-}
 
-string Weather::get_name() const{
+string Weather::get_name() const {
     return station_nm;
 }
 
-int Weather::get_rating() const{
-	return rating;
-}
-
-void Weather::set_rating(int new_rating) {
-	rating = new_rating;
-}
-
-void Weather::add_reading(WReading wr){
-	wreadings.push_back(wr);
-}
-
-ostream& operator<<(ostream& os, const Weather& w) {
-	int r = w.get_rating();
-	os << w.station_nm << ": rating " << r << w.my_loc << endl;
-	for(WReading a : w.wreadings) {
-		os << a;
-	}
-	return os;
-}
-
-ostream& operator<<(ostream& os, const Date& d) {
-	os << d.month << "/" << d.day << "/" << d.year << endl;
-	return os;
-}
-
-ostream& operator<<(ostream& os, const WReading& r) {
-	os << "Date: " << r.date << " Temperature: " << r.temperature << "Humidity: " << r.humidity << "Windspeed: " << r.windspeed << endl;
-	return os;
-}
+void Weather::add_reading(WReading wr) { }
