60a21f748855583014bfe26702dd9dc7ac7ef1d3
Da Yeon <dayeonshin0315@gmail.com>
2019-06-13 11:19:44 -0400

    complex

--- tests/test_weather.cpp
+++ tests/test_weather.cpp
@@ -6,20 +6,20 @@
 
 using namespace std;
 
-void get_wreadings(string filenm, Weather& w) {
-    ifstream rfile(filenm);
-    if (!rfile) {
-        cout << "Could not read input file: " << filenm << endl;
-        exit(1);
-    }
-    int m, d, y;
-    double temp, hum, ws;
-    while (rfile >> m >> d >> y >> temp >> hum >> ws) {
-        WReading wr = WReading(Date(d, m, y), temp, hum, ws);
-        w.add_reading(wr);
-    }
-    rfile.close();
-}
+// void get_wreadings(string filenm, Weather& w) {
+//     ifstream rfile(filenm);
+//     if (!rfile) {
+//         cout << "Could not read input file: " << filenm << endl;
+//         exit(1);
+//     }
+//     int m, d, y;
+//     double temp, hum, ws;
+//     while (rfile >> m >> d >> y >> temp >> hum >> ws) {
+//         WReading wr = WReading(Date(d, m, y), temp, hum, ws);
+//         w.add_reading(wr);
+//     }
+//     rfile.close();
+// }
 
 void make_images1(Image& img1) {
     for (int i = 0; i < 100; i++) {
@@ -44,25 +44,25 @@ int main() {
     }
     exit(0);
 
-    Date date1 = Date(1, 1, 2019);
-    WReading test_temps = WReading(date1, 0, 50, 10);
-    int wr_size = sizeof(test_temps);
-    cout << "Size of a Date is " << sizeof(date1) << endl;
-    cout << "Size of a WReading is " << wr_size << endl;
+    // Date date1 = Date(1, 1, 2019);
+    // WReading test_temps = WReading(date1, 0, 50, 10);
+    // int wr_size = sizeof(test_temps);
+    // cout << "Size of a Date is " << sizeof(date1) << endl;
+    // cout << "Size of a WReading is " << wr_size << endl;
 
-    double freezingF = test_temps.get_tempF();
-    assert(freezingF == 32.0);
-    test_temps = WReading(Date(1, 1, 2019), 100, 50, 10);
-    freezingF = test_temps.get_tempF();
-    assert(freezingF == 212.0);
+    // double freezingF = test_temps.get_tempF();
+    // assert(freezingF == 32.0);
+    // test_temps = WReading(Date(1, 1, 2019), 100, 50, 10);
+    // freezingF = test_temps.get_tempF();
+    // assert(freezingF == 212.0);
 
-    Weather irkutsk = Weather("Irkutsk", GPS(46.3, 67.2));
+    // Weather irkutsk = Weather("Irkutsk", GPS(46.3, 67.2));
 
-    string filenm;
-    cout << "Input the name of the new readings file:\n";
-    cin >> filenm;
+    // string filenm;
+    // cout << "Input the name of the new readings file:\n";
+    // cin >> filenm;
 
-    get_wreadings(filenm, irkutsk);
+    // get_wreadings(filenm, irkutsk);
 
 //    cout << irkutsk << endl;
 }
