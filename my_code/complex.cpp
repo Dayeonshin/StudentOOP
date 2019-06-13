318b5f4861297302528c2e8fb31a4e8ba90607a7
Da Yeon <dayeonshin0315@gmail.com>
2019-06-13 13:22:34 -0400

    Complex

--- my_code/complex.cpp
+++ my_code/complex.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <istream>
 #include <iomanip>
+#include <cmath>
 #include "complex.h"
 
 using namespace std;
 
 bool operator== (const Complex& c1, const Complex& c2) {
-	if(c1.get_imag() - c2.get_imag() < 0.0001) {
-		if (c2.get_real() - c2.get_real() < 0.0001) {
-			return false;
+	if(abs(c1.get_imag() - c2.get_imag()) < 0.0001) {
+		if (abs(c2.get_real() - c2.get_real()) < 0.0001) {
+			return true;
 		}
 	}
-	return true;
+	return false;
 }
 
 ostream& operator<< (ostream& os, const Complex& c) {
@@ -43,6 +44,37 @@ Complex Complex::operator++(int dummy) {
 	Complex temp(*this);
 	real++;
 	return temp;
+}
+
+Complex& Complex::operator--() {
+	--real;
+	return (*this);
+}
+
+Complex Complex::operator--(int dummy) {
+	Complex temp(*this);
+	real--;
+	return temp;
+}
+
+Complex operator+(const Complex& c1, const Complex& c2) {
+	double real_temp = c1.get_real() + c2.get_real();
+	double real_imag = c1.get_imag() + c2.get_imag();
+	Complex c(real_temp, real_imag);
+	return c;
+}
+
+Complex operator-(const Complex& c1, const Complex& c2) {
+	double real_temp = c1.get_real() - c2.get_real();
+	double real_imag = c1.get_imag() - c2.get_imag();
+	Complex c(real_temp, real_imag);
+	return c;
+}
+
+Complex Complex::operator-=(const Complex& c) {
+		real -= c.real;
+		imag -= c.imag;
+		return *this;
 }
 
 double Complex::get_real() const {
